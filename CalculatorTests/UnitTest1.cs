0d51543363f207a9d9b8087c7f13896914a28900
user452 <user452@PK452-1>
2022-06-17 16:37:17 +0500

    Добавьте файлы проекта.

---
+++ CalculatorTests/UnitTest1.cs
@@ -0,0 +1,69 @@
+using NUnit.Framework;
+using ConsoleApp_калькулятор_Туленцева;
+namespace CalculatorTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Plus_1plus6_7()
+        {
+            Calculator calc = new Calculator();
+            double result = calc.Plus(1, 6);
+            Assert.AreEqual(7,result);
+        }
+        [Test]
+        public void Minus_6minus1_5()
+        {
+            Calculator calc = new Calculator();
+            double result = calc.Minus(6, 1);
+            Assert.AreEqual(5, result);
+        }
+        [Test]
+        public void Multiplication_6multi1_6()
+        {
+            Calculator calc = new Calculator();
+            double result = calc.Multiplication(6, 1);
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void Divide_6divide2_3()
+        {
+            Calculator calc = new Calculator();
+            double result = calc.Divide(6, 2);
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void TriangleSquare_4multi_5_10()
+        {
+            Calculator calc = new Calculator();
+            double result = calc.TriangleSquare(4, 5);
+            Assert.AreEqual(10, result);
+        }
+
+        [Test]
+        public void RoundSquare_PImulti_2()
+        {
+            Calculator calc = new Calculator();
+            double result = calc.RoundSquare(2);
+            Assert.GreaterOrEqual(result, 12);
+        }
+
+        [Test]
+        public void Average_12345_3()
+        {
+            Calculator calc = new Calculator();
+            double [] array = new double[] { 1,2,3,4,5};
+            double result = calc.Average(array);      
+            Assert.AreEqual(3,result);
+        }
+
+
+    }
+}
---
+++ ConsoleApp калькулятор Туленцева/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleApp_калькулятор_Туленцева
+{
+    public class Calculator
+    {
+        public double Plus(double a, double b)
+        {
+            double result = a + b;
+            return result;
+        }
+        public double Minus(double a, double b)
+        {
+            double result = a - b;
+            return result;
+        }
+        public double Multiplication(double a, double b)
+        {
+            double result = a * b;
+            return result;
+        }
+        public double Divide(double a, double b)
+        {
+            double result = a / b;
+            return result;
+        }
+
+        public double TriangleSquare(double a, double h)
+        {
+            double result = 0.5d * a * h;
+            return result;
+        }
+
+        public double RoundSquare(double r)
+        {
+
+            double result = Math.PI * r * r;
+            return result;
+        }
+
+        public double Average(double [] array)
+        { double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+        {
+                sum += array[i]; 
+            }
+            return sum / array.Length;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            double res = calc.Plus(1, 6);
+            Console.WriteLine($"Результат: {res}");
+        }
+    }
+}
